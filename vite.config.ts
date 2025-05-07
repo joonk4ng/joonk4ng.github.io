e5919bae4f857a6f7a886e637dc962863856f42f
joonk4ng <joonhyunga@gmail.com>
2025-05-06 17:50:06 -0700

    fix asset loading and sw

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,10 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
   },
