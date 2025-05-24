27a6251084e28f6348cc483648cc804097097f90
Joon Kang <jkang@privser.com>
2025-05-23 18:20:02 -0700

    Clean deploy for GitHub Pages

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MainTable from './components/MainTable';
-
-export default function App() {
-  return <MainTable />;
-} 
--- src/types/pdf.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'pdfjs-dist/build/pdf.worker.entry' {
-    const content: any;
-    export default content;
-} 
