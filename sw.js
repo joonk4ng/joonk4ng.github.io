76884381c33ae14265022f36d5ae993e6ce6b0c3
Christopher Wilson <christopherwilson@Mac-1.local>
2025-07-18 15:34:53 -0700

    Updates

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './pdf.worker.min.mjs',
+  './pdf.worker.mjs',
   './CTR_Fillable_Edited.pdf',
   './pdfjskit/pdfjskit.min.css',
   './pdfjskit/pdfjs/web/viewer.mjs',
